b215ac789e1f419d0494191f9dd7c4c2ee33c67d
Chris <krzysiek.palonek@gmail.com>
2023-05-09 21:56:07 +0200

    09.05

---
+++ WebAPI.Tests/QuizApiTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.DTO;
+using Xunit;
+
+namespace WebAPI.Tests;
+
+public class QuizApiTest
+{
+    [Fact]
+    public async void GetShouldReturnOneQuizz()
+    {
+        //Arrange
+        await using var application = new WebApplicationFactory<Program>();
+        using var client = application.CreateClient();
+
+        //Act
+        var result = await client.GetFromJsonAsync<List<QuizDto>>( "/Quizes" );
+
+        //Assert
+        Assert.Equal( 1, result.Count );
+    }
+
+    [Fact]
+    public async void GetShouldReturnOkStatus()
+    {
+        //Arrange
+        await using var application = new WebApplicationFactory<Program>();
+        using var client = application.CreateClient();
+
+        //Act
+        var result = await client.GetAsync( "/Quizes" );
+
+        //Assert
+        Assert.Equal( HttpStatusCode.OK, result.StatusCode );
+        Assert.Contains( "application/json", result.Content.Headers.GetValues( "Content-Type" ).First() );
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -86,3 +86,6 @@ app.MapControllers();
 app.Seed();
 app.AddUsers();
 app.Run();
+
+public partial class Program
+{ }
